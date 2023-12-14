e442add5bf0e1ca65ca54bab92e721f3abdd280e
Alexandru Bereghici <abereghici@users.noreply.github.com>
2023-12-14 15:06:33 +0200

    Update 14.ts

--- 14.ts
+++ 14.ts
@@ -1,3 +1,9 @@
-type DecipherNaughtyList<S, R extends string[] = []> = S extends `${infer F}/${infer L}`
-	? DecipherNaughtyList<L, [...R, F]>
-	: R[number] | S;
+// First version: 
+
+// type DecipherNaughtyList<S, R extends string[] = []> = S extends `${infer F}/${infer L}`
+// 	? DecipherNaughtyList<L, [...R, F]>
+// 	: R[number] | S;
+
+// Updated version: 
+
+type DecipherNaughtyList<S> = S extends `${infer F}/${infer L}` ? DecipherNaughtyList<L> | F : S;
